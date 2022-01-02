353a8712bf60dc9e28b39c5167ccb9dc5df9c514
alex <alex.pajaron@vier.ai>
2022-01-02 21:53:28 +0100

    add varargs overloaded method

--- src/main/scala/intro/Upper.scala
+++ src/main/scala/intro/Upper.scala
@@ -4,4 +4,7 @@ object Upper {
   def upper(value: String): String = value.toUpperCase
 
   def convert(strings: List[String]): List[String] = strings.map(_.toUpperCase).toList
+
+  def upper(params: String*): String =
+    params.map(_.toUpperCase).mkString(" ")
 }
--- src/test/scala/intro/UpperTest.scala
+++ src/test/scala/intro/UpperTest.scala
@@ -3,6 +3,8 @@ package intro
 import org.junit.Assert.{assertEquals, assertSame}
 import org.junit.Test
 
+import scala.runtime.Arrays
+
 class UpperTest {
 
   @Test def testUpper(): Unit =
@@ -10,6 +12,9 @@ class UpperTest {
 
   @Test def testConvert(): Unit =
     assertEquals(List("HELLO","BYE"), Upper.convert(List("hello", "bye")))
+
+  @Test def testUpperGivenParams(): Unit =
+    assertEquals("HELLO BYE", Upper.upper("hello", "bye"))
 
 
 }
