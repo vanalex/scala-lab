4bbcd0f373feb5c066998b0f839ab288cd26b421
alex <alex.pajaron@vier.ai>
2022-01-08 22:06:44 +0100

    more examples on Option

--- src/test/scala/standard/OptionsTest.scala
+++ src/test/scala/standard/OptionsTest.scala
@@ -26,4 +26,31 @@ class OptionsTest extends AnyFlatSpec with Matchers{
     value2 getOrElse "No value" should be("No value")
     value2 getOrElse {"default function"} should be("default function")
   }
+
+  it should "Option with pattern matching" in {
+    val someValue: Option[Double] = Some(20.0)
+    val value = someValue match {
+      case Some(v) => v
+      case None => 0.0
+    }
+
+    value should be(20.0)
+
+    val noValue: Option[Double] = None
+    val value1 = noValue match {
+      case Some(v) => v
+      case None => 0.0
+    }
+    value1 should be(0.0)
+  }
+
+  it should "Option with collecton style operations" in {
+    val number: Option[Int] = Some(3)
+    val noNumber: Option[Int] = None
+    val result1 = number.map(_ * 1.5)
+    val result2 = noNumber.map(_ * 1.5)
+
+    result1 should be(Some(4.5))
+    result2 should be(None)
+  }
 }
