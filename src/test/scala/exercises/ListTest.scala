9957d74e570b2920ca33b96cdf33cf65dba3ccc0
alex <alex.pajaron@vier.ai>
2022-01-20 23:29:01 +0100

    examples on list immutability

--- src/test/scala/exercises/ListTest.scala
+++ src/test/scala/exercises/ListTest.scala
@@ -44,4 +44,12 @@ class ListTest extends AnyFlatSpec with Matchers{
     b(2) should equal(5)
     b(4) should equal(9)
   }
+
+  it should("test Lists are immutable") in {
+    val a = List(1, 3, 5, 7, 9)
+    val b = a.filterNot(v => v == 5) // remove where value is 5
+
+    a should equal(List(1, 3, 5, 7, 9))
+    b should equal(List(1, 3, 7, 9))
+  }
 }
