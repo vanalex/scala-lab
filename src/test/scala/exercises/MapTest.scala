0e39e7fd8c568ab1777ea1bdd8580b9ad3c68a57
alex <alex.pajaron@vier.ai>
2022-02-03 22:55:40 +0100

    beginning map tests

--- src/test/scala/exercises/MapTest.scala
+++ src/test/scala/exercises/MapTest.scala
@@ -9,4 +9,15 @@ class MapTest extends AnyFlatSpec with Matchers {
     val myMap = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "IA" -> "Iowa")
     myMap.size should be(4)
   }
+
+  it should "test map should not contain idential items" in {
+    val myMap = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "MI" -> "Michigan")
+    myMap.size should be(3)
+  }
+
+  it should "test map item can be added to easily" in {
+    val myMap = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "MI" -> "Michigan")
+    val aNewMap = myMap + ("IL" -> "Illinois")
+    aNewMap.contains("IL") should be(true)
+  }
 }
