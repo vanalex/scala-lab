ddb469c3b8b8c3600b6ad0fdea4545f1fd2c6f10
alex <alex.pajaron@vier.ai>
2022-02-08 21:39:54 +0100

    starting set tests

--- src/test/scala/exercises/SetTest.scala
+++ src/test/scala/exercises/SetTest.scala
@@ -9,4 +9,24 @@ class SetTest extends AnyFlatSpec with Matchers{
       val mySet = Set("Michigan", "Ohio", "Wisconsin", "Iowa")
       mySet.size should be(4)
     }
+
+    it should "test sets contain distinct values" in {
+      val mySet = Set("Michigan", "Ohio", "Wisconsin", "Michigan")
+      mySet.size should be(3)
+    }
+
+    it should "test Sets can be added to easily" in {
+      val mySet = Set("Michigan", "Ohio", "Wisconsin", "Iowa")
+      val aNewSet = mySet + "Illinois"
+
+      aNewSet.contains("Illinois") should be(true)
+      mySet.contains("Illinois") should be(false)
+    }
+
+  it should "test sets may be of mixed type:" in {
+    val mySet = Set("Michigan", "Ohio", 12)
+
+    mySet.contains(12) should be(true)
+    mySet.contains("MI") should be(false)
+  }
 }
