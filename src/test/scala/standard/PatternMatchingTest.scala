62ede781e2e2e0a8f83fd2b483c8e2d06b6c2f66
alex <alex.pajaron@vier.ai>
2022-02-21 22:53:17 +0100

    pattern matching another example

--- src/test/scala/standard/PatternMatchingTest.scala
+++ src/test/scala/standard/PatternMatchingTest.scala
@@ -5,8 +5,25 @@ import org.scalatest.matchers.should.Matchers
 
 class PatternMatchingTest extends AnyFlatSpec with Matchers{
 
-  it should "test set size" in {
+  it should "test simple pattern mathcing" in {
     var result = PatternMatchingUtil.matchTest(3);
     result should be("many")
+  }
+
+  it should "test other simple pattern matching" in {
+    val stuff = "blue"
+
+    val myStuff = stuff match {
+      case "red" =>
+        println("RED"); 1
+      case "blue" =>
+        println("BLUE"); 2
+      case "green" =>
+        println("GREEN"); 3
+      case _ =>
+        println(stuff); 0 // case _ will trigger if all other cases fail.
+    }
+
+    myStuff should be(2)
   }
 }
