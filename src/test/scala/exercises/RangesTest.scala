dc2a7bed99cbe0ad1c84e2a54e4bd151fdd7a3fe
alex <alex.pajaron@vier.ai>
2022-04-14 23:01:10 +0200

    ranges examples

--- src/test/scala/exercises/RangesTest.scala
+++ src/test/scala/exercises/RangesTest.scala
@@ -14,4 +14,21 @@ class RangesTest extends AnyFlatSpec with Matchers{
     second should be(1)
     last should be(9)
   }
+
+  it should "Ranges can be specified using 'until'" in {
+    val someNumbers = Range(0, 10)
+    val otherRange = 0 until 10
+
+    (someNumbers == otherRange) should be(true)
+  }
+
+  it should "Range can specify a step for an increment" in {
+    val someNumbers = Range(2, 10, 3)
+    val second = someNumbers(1)
+    val last = someNumbers.last
+
+    someNumbers.size should be(3)
+    second should be(5)
+    last should be(8)
+  }
 }
