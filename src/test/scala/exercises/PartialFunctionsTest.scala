71834bd398ee67380df0460b0f058fe9f1ff4978
alex <alex.pajaron@vier.ai>
2022-04-19 22:58:35 +0200

    partial functions examples

--- src/test/scala/exercises/PartialFunctionsTest.scala
+++ src/test/scala/exercises/PartialFunctionsTest.scala
@@ -14,4 +14,17 @@ class PartialFunctionsTest extends AnyFlatSpec with Matchers{
     whatToDo(3) should be(9)
     whatToDo(4) should be(8)
   }
+
+  it should "case partial function" in {
+    val doubleEvens: PartialFunction[Int, Int] = {
+      case x if (x % 2) == 0 => x * 2
+    }
+    val tripleOdds: PartialFunction[Int, Int] = {
+      case x if (x % 2) != 0 => x * 3
+    }
+
+    val whatToDo = doubleEvens orElse tripleOdds //Here we chain the partial functions together
+    whatToDo(3) should be(9)
+    whatToDo(4) should be(8)
+  }
 }
